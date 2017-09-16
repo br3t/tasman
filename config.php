2fb11f17404b86e702a1606ded87069df130883c
br3t <bret.snk@gmail.com>
2017-09-16 11:39:25 +0300

    Replace dummy credentials width empty one

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 	$DB_HOST = "127.0.0.1";
-	$DB_NAME = "tasman";
-	$DB_USER = "tasman";
-	$DB_PASSWORD = "1111";
+	$DB_NAME = "";
+	$DB_USER = "";
+	$DB_PASSWORD = "";
 	$DB_CHARSET = "utf8";
 ?>
