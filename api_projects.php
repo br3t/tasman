0e598d3e09b1e6b9cc619a03e97177f5bc0d8e70
br3t <bret.snk@gmail.com>
2019-04-24 17:54:37 +0300

    Some fixes

--- api_projects.php
+++ api_projects.php
@@ -10,40 +10,47 @@ if(!$user->get_is_logged()) {
 
 } else {
 
-	if(isset($_GET['action']) && isset($_GET['entity'])) {
+	if(isset($_GET['action']) && isset($_GET['entity']) && $_GET['entity'] == "project") {
 		// Check "routes"
-		if($_GET['action'] == "get" && $_GET['entity'] == "project") {
-			//--------------
-			//* get projects info
-			$filtered = $user->get_id();
-			if($user->get_role() == 1) {
-				$filtered = 'all';
-			}
-			$ajax_respond = get_all_projects($filtered, $ajax_respond);
-			$ajax_respond = get_all_tasks($ajax_respond);
+		switch ($_GET['action']) {
+			case 'get':
+				//--------------
+				//* get projects info
+				$filtered = $user->get_id();
+				if($user->get_role() == 1) {
+					$filtered = 'all';
+				}
+				$ajax_respond = get_all_projects($filtered, $ajax_respond);
+				$ajax_respond = get_all_tasks($ajax_respond);
+				break;
 
-		} else if($_GET['action'] == "create" && $_GET['entity'] == "project") {
-			//--------------
-			//* add new project
-			$insert_data = array(
-				'name' => htmlspecialchars($_GET['name']),
-				'owner_id' => $user->get_id()
-			);
-			$ajax_respond = create_project($insert_data, $ajax_respond);
+			case 'create':
+				//--------------
+				//* add new project
+				$insert_data = array(
+					'name' => htmlspecialchars($_GET['name']),
+					'owner_id' => $user->get_id()
+				);
+				$ajax_respond = create_project($insert_data, $ajax_respond);
+				break;
 
-		} else if($_GET['action'] == "edit" && $_GET['entity'] == "project") {
-			//--------------
-			//* edit project
-			$update_data = array(
-				'name' => trim(htmlspecialchars($_GET['name'])),
-				'project_id' => intval($_GET['project_id'])
-			);
-			$ajax_respond = update_project($update_data, $ajax_respond);
-		} else if($_GET['action'] == "remove" && $_GET['entity'] == "project") {
-			//--------------
-			//* remove project
-			$project_id = intval($_GET['project_id']);
-			$ajax_respond = remove_project($project_id, $ajax_respond);
+			case 'edit':
+				//--------------
+				//* edit project
+				$update_data = array(
+					'name' => trim(htmlspecialchars($_GET['name'])),
+					'project_id' => intval($_GET['project_id'])
+				);
+				$ajax_respond = update_project($update_data, $ajax_respond);
+				break;
+
+			case 'remove':
+				//--------------
+				//* remove project
+				$project_id = intval($_GET['project_id']);
+				$ajax_respond = remove_project($project_id, $ajax_respond);
+				break;
+
 		}
 	}
 	
